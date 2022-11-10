c55066d930b083408cf90215f530b6f006fe3432
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2022-11-11 01:01:10 +0700

    Switched to MinesweeperCell for easy storage

--- minesweeper.hpp
+++ minesweeper.hpp
@@ -1,9 +1,21 @@
-typedef struct Minesweeper {
-    unsigned short int rows;
-    unsigned short int cols;
-    unsigned short int mines;
+#include <vector>
 
-    short int **board;
-    char **mask;
-
-} Minesweeper;
+enum flag_status
+{
+    /**
+     * Defines the status for the current Cell.
+     */
+    UNFLAGGED = 0,
+    FLAGGED = 1,
+    UNSURE = 2
+};
+typedef struct MinesweeperCell
+{
+    /**
+     * Defines a Cell in the Minesweeper game.
+     */
+    bool mine = false;                // If it's a mine.
+    bool revealed = false;            // If it has been revealed.
+    flag_status flagged = UNFLAGGED;  // If it has been flagged, defined above.
+    unsigned short int neighbors = 0; // How many mines are neighboring the square.
+} MinesweeperCell;
