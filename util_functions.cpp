d86642a276ba4a07646f1cee9c8a174f671ba497
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2022-11-16 23:16:04 +0700

    Tidied the code

--- util_functions.cpp
+++ util_functions.cpp
@@ -3,73 +3,161 @@
 #include <iostream>
 #include <random>
 #include <tuple>
+#include <chrono>
+#include <thread>
+
+#include "minesweeper.hpp"
+
 int random_num_gen(int from, int to)
 {
-  /**
-   * A *really really* overkill number generator function, using the Mersenne
-   * Twister engine. Generates an integer ranging `from` to `to`,
-   * exclusively. Reference:
-   * https://learn.microsoft.com/en-us/cpp/standard-library/random?view=msvc-170
-   *
-   * @param from The starting number of the range
-   * @param to The ending number of the range
-   *
-   * @return a new random integer
-   */
+    /**
+     * A *really really* overkill number generator function, using the Mersenne
+     * Twister engine. Generates an integer ranging `from` to `to`,
+     * exclusively. Reference:
+     * https://learn.microsoft.com/en-us/cpp/standard-library/random?view=msvc-170
+     *
+     * @param from The starting number of the range
+     * @param to The ending number of the range
+     *
+     * @return a new random integer
+     */
 
-  std::random_device rd;
-  std::mt19937 rng_engine(rd());
-  std::uniform_int_distribution<int> dist(from, to - 1);
+    std::random_device rd;
+    std::mt19937 rng_engine(rd());
+    std::uniform_int_distribution<int> dist(from, to - 1);
 
-  return dist(rng_engine);
+    return dist(rng_engine);
 }
 void print_help_sel()
 {
-  std::cout << "Pick a grid preset: \n";
-  std::cout << "0. Custom (default)\n";
-  std::cout << "1. Easy - 8x8 - 10 mines\n";
-  std::cout << "2. Intermediate - 16x16 - 40 mines\n";
-  std::cout << "3. Hard - 30x16 - 99 mines\n";
+    std::cout << "Pick a grid preset: \n";
+    std::cout << "0. Custom (default)\n";
+    std::cout << "1. Easy - 8x8 - 10 mines\n";
+    std::cout << "2. Intermediate - 16x16 - 40 mines\n";
+    std::cout << "3. Hard - 30x16 - 99 mines\n";
 }
 std::tuple<int, int, int> difficulty()
 {
-  unsigned int height, width;
-  unsigned int mines;
-  unsigned int preset = 0;
+    unsigned int height, width;
+    unsigned int mines;
+    unsigned int preset = 0;
 
-  print_help_sel();
-  std::cin >> preset;
-
-  while (preset < 0 || preset > 3) {
-    std::cout << "That option is unrecognized!\n";
-    Sleep(2000);
     print_help_sel();
     std::cin >> preset;
-  }
 
-  if (preset == 0) {
-    std::cout << "Enter the height of the grid: ";
-    std::cin >> height;
-    std::cout << "Enter the width of the grid: ";
-    std::cin >> width;
-    std::cout << "Enter the number of mines to randomize: ";
-    std::cin >> mines;
-  }
-  else if (preset == 1) {
-    height = 8;
-    width = 8;
-    mines = 10;
-  }
-  else if (preset == 2) {
-    height = 16;
-    width = 16;
-    mines = 40;
-  }
-  else if (preset == 3) {
-    height = 40;
-    width = 16;
-    mines = 99;
-  }
+    while (preset < 0 || preset > 3) {
+        std::cout << "That option is unrecognized!\n";
+        Sleep(2000);
+        print_help_sel();
+        std::cin >> preset;
+    }
 
-  return std::make_tuple(height, width, mines);
+    if (preset == 0) {
+        std::cout << "Enter the height of the grid: ";
+        std::cin >> height;
+        std::cout << "Enter the width of the grid: ";
+        std::cin >> width;
+        std::cout << "Enter the number of mines to randomize: ";
+        std::cin >> mines;
+    }
+    else if (preset == 1) {
+        height = 8;
+        width = 8;
+        mines = 10;
+    }
+    else if (preset == 2) {
+        height = 16;
+        width = 16;
+        mines = 40;
+    }
+    else if (preset == 3) {
+        height = 40;
+        width = 16;
+        mines = 99;
+    }
+
+    return std::make_tuple(height, width, mines);
+}
+
+std::vector<std::vector<MinesweeperCell> > create_game_field(int rows, int cols,
+                                                             int mines)
+{
+    /**
+     * Creates the `board`, which contains the cell data and mine positions,
+     *
+     * @param rows Number of rows
+     * @param cols Number of mines
+     * @param mines Number of mines
+     *
+     * @return still have no idea yet
+     */
+
+    // Creates the solution field
+    std::vector<std::vector<MinesweeperCell> > table(
+        rows, std::vector<MinesweeperCell>(cols));
+
+    // Fill in the board with MinesweeperCells
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            table[i][j] = MinesweeperCell();
+        }
+    }
+
+    // Generate the mine positions:
+    for (int current_mine = 0; current_mine < mines; current_mine++) {
+        int random_bomb_row = random_num_gen(0, rows);
+        int random_bomb_col = random_num_gen(0, cols);
+
+        // Check if the selected position has a mine already
+        if (table[random_bomb_row][random_bomb_col].mine == true) {
+            continue;
+        }
+        else {     // Set the randomized position to be a mine
+            table[random_bomb_row][random_bomb_col].mine = true;
+
+            // Find all the neighboring cells of the mine, then add up
+            // the neighbor counts for each neighboring cells
+            if (random_bomb_row != 0 &&
+                random_bomb_col != 0)     // check for edge
+                table[random_bomb_row - 1][random_bomb_col - 1].neighbors += 1;
+
+            if (random_bomb_row != 0)     // check for edge
+                table[random_bomb_row - 1][random_bomb_col].neighbors += 1;
+
+            if (random_bomb_row != 0 &&
+                random_bomb_col != cols - 1)     // check for edge
+                table[random_bomb_row - 1][random_bomb_col + 1].neighbors += 1;
+
+            if (random_bomb_col != 0)     // check for edge
+                table[random_bomb_row][random_bomb_col - 1].neighbors += 1;
+
+            if (random_bomb_col != cols - 1)     // check for edge
+                table[random_bomb_row][random_bomb_col + 1].neighbors += 1;
+
+            if (random_bomb_row != rows - 1 &&
+                random_bomb_col != 0)     // check for edge
+                table[random_bomb_row + 1][random_bomb_col - 1].neighbors += 1;
+
+            if (random_bomb_row != rows - 1)     // check for edge
+                table[random_bomb_row + 1][random_bomb_col].neighbors += 1;
+
+            if (random_bomb_row != rows - 1 &&
+                random_bomb_col != cols - 1)     // check for edge
+                table[random_bomb_row + 1][random_bomb_col + 1].neighbors += 1;
+        }
+    }
+
+    return table;
+}
+std::tuple<int, int> ask_position()
+{
+    int row, col;
+    std::cout << "Enter your desired position (row, column): ";
+    std::cin >> row >> col;
+    return std::make_tuple(row - 1, col - 1);
+}
+
+void clear_screen() { std::cout << "\033[2J\033[1;1H"; }
+void sleep(int milliseconds) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
 }
