69357557962353c8acd21f3a814080935c00ff2c
Rookie Nguyen <nguyenquocthang2004@gmail.com>
2023-01-01 12:58:43 +0700

    Added welcome banner

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ std::vector<std::vector<MinesweeperCell>> game_field;
 
 int main(int argc, char *argv[])
 {
+  print_ascii_banner();
     /**
      * @brief The main function.
      * Still does a lot of things before we can actually play the game.
@@ -265,10 +266,7 @@ void ask_for_replay()
     }
     else if (sel == 'n' || sel == 'N' || sel == ' ')
     {
-        clear_screen();
-        std::cout << "Thank you for playing!\n";
-        sleep(1500);
-        exit(0);
+        quit_game();
     }
 }
 
@@ -422,4 +420,24 @@ void load_saved_game()
         start_new_game();
         sleep(1500);
     }
+}
+void print_ascii_banner() {
+    /**
+     * @brief Prints out a very nice ASCII art banner.
+    */
+    std::cout << R"(
+8888888b.                 888     d8b         .d8888b.                                                      
+888   Y88b                888     Y8P        d88P  Y88b                                                     
+888    888                888                Y88b.                                                          
+888   d88P .d88b.  .d88b. 888  888888 .d88b.  "Y888b.  888  888  888 .d88b.  .d88b. 88888b.  .d88b. 888d888 
+8888888P" d88""88bd88""88b888 .88P888d8P  Y8b    "Y88b.888  888  888d8P  Y8bd8P  Y8b888 "88bd8P  Y8b888P"   
+888 T88b  888  888888  888888888K 88888888888      "888888  888  8888888888888888888888  88888888888888     
+888  T88b Y88..88PY88..88P888 "88b888Y8b.    Y88b  d88PY88b 888 d88PY8b.    Y8b.    888 d88PY8b.    888     
+888   T88b "Y88P"  "Y88P" 888  888888 "Y8888  "Y8888P"  "Y8888888P"  "Y8888  "Y8888 88888P"  "Y8888 888     
+                                                                                    888                     
+                                                                                    888                     
+                                                                                    888
+
+)";
+    std::cout << "Made by Nguyen Huu Quoc Thang (Rookie Nguyen)\n";
 }
